0de1b443f76a268cae29dcc1c9eaebad85a33b18
Maxime SERREE <maxserree@free.fr>
2019-09-30 12:22:54 +0200

    Update NodeTest.java

--- src/test/java/graph/NodeTest.java
+++ src/test/java/graph/NodeTest.java
@@ -1,15 +1,50 @@
-package projetGraf;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+package graph;
 
 import graph.Node;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for node.
  */
 public class NodeTest {
+
+    //Valeur(s) globale(s)
+    Node node1;
+
+    @Before
+    public void begin(){
+        node1 = new Node("node1",1);
+    }
+
+    @Test
+    public void constructorNull_OK(){
+        Node nodeNull = new Node();
+        assertEquals(nodeNull.getName(),"");
+        assertEquals(nodeNull.getNumber(),-1);
+
+    }
+
+    @Test
+    public void toStringNode_OK(){
+        assertEquals(node1.toString(),"1");
+    }
+
+    @Test
+    public void toStringNode_KO(){
+        assertNotEquals(node1.toString(),"2");
+    }
+
+
+    @Test
+    public void getName_OK(){
+        assertEquals(node1.getName(),"node1");
+    }
+
+
 
     @Test
     public void check2NodeEquals() {
